39e1a5b0a1d03ceab5fcd50d32cd4fc8d126f937
Philipp Salvisberg <philipp.salvisberg@gmail.com>
2025-05-04 16:44:35 +0200

    fix d2-2150-sol.sql, remove unnecessary variable

--- demo-001/d2-2150-sol.sql
+++ demo-001/d2-2150-sol.sql
@@ -11,10 +11,9 @@ end;
 /
 
 declare
-   l_comm emp.comm%type;
 begin
    <<print_emps_without_comm>>
-   for r in (select ename from emp where job = 'SALESMAN' and (comm = l_comm or l_comm is null))
+   for r in (select ename from emp where job = 'SALESMAN' and l_comm is null)
    loop
       sys.dbms_output.put_line(r.ename || ' has no commission');
    end loop print_emps_without_comm;
